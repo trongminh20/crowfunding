51a06725463c01a2cf79fd1b7cb51db96360f885
Minh Phan <trongminh20@gmail.com>
2021-07-01 01:15:03 -0700

    change color of responsive button

--- mainJS.js
+++ mainJS.js
@@ -24,9 +24,7 @@ function showFooter(e) {
 function showModel() {
   let target = document.getElementById("models-reward");
   if (target.style.display == 'none') {
-    // document.style = "position:relative;";
-    
-    // document.body.style = " -webkit-filter: blur(2px);-moz-filter: blur(2px);-o-filter: blur(2px);-ms-filter: blur(2px);filter:blur(2px);";
+
     document.style = "position:relative;";
     target.style = "position:absolute;z-index:12; display:flex; flex-direction:column;top:100%;right:.25rem;";
   }else{
@@ -37,6 +35,3 @@ function closeModel(){
   document.getElementById('models-reward').style.display = "none";
 }
 
-function scrollNavbar(e){
-  e.classList.add("scrolling");
-}
